bf071311bf8c874013d41c9cd94a6187175c5fdb
Maz <markuspietila95@gmail.com>
2020-03-10 17:49:59 +0200

    Updates

---
+++ precache-manifest.b504e68c0182ab4a3c3ed5e2c21ebfe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d796d80cab5acc227660aac4709d6c",
+    "url": "/KeyFinder/index.html"
+  },
+  {
+    "revision": "b2b7f23d0cd1b851d546",
+    "url": "/KeyFinder/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6ce1ddf0970cf24d05e5",
+    "url": "/KeyFinder/static/css/main.42c4aeba.chunk.css"
+  },
+  {
+    "revision": "b2b7f23d0cd1b851d546",
+    "url": "/KeyFinder/static/js/2.6f3d0d26.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/KeyFinder/static/js/2.6f3d0d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce1ddf0970cf24d05e5",
+    "url": "/KeyFinder/static/js/main.2ea54fe5.chunk.js"
+  },
+  {
+    "revision": "7a498d82bad4fbcb4540",
+    "url": "/KeyFinder/static/js/runtime-main.c4fc74fe.js"
+  },
+  {
+    "revision": "e65e852d419f2143ef3144f3ab26d61d",
+    "url": "/KeyFinder/static/media/fretboard.e65e852d.png"
+  }
+]);
